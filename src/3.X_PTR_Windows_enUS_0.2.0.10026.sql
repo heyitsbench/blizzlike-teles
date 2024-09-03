2a3874c629876201402d7fc0e88b238c24dc3a8c
heyitsbench <benjymansy123@gmail.com>
2024-09-03 15:15:52 -0400

    Remove meta-queries.

--- src/3.X_PTR_Windows_enUS_0.2.0.10026.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE TABLE `game_tele`;
-SET @ID = 10026;
-INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
-(@ID+1, 795.754, 618.234,  412.392, 181.26,  650, 'Argentdungeon'),
-(@ID+2, 510.773, 139.338,  395.206, 359.865, 649, 'Argentraid'),
-(@ID+3, 471.273, -836.112, 44.1175, 20.7672, 628, 'northrendbg2');
--- src/3.X_PTR_Windows_enUS_0.2.0.10048.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE `game_tele`;
-SET @ID = 10048;
-INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
-(@ID+1, 795.754, 618.234, 414,     181.26,  650, 'Argentdungeon'),
-(@ID+2, 563.887, 84.0215, 395.203, 90.3151, 649, 'Argentraid');
